69f0b863ea9410c5ea8e56b734816f715dfa8d7f
Prasanna Sekar <prasanna.sekar@sacumen.com>
2025-08-16 17:14:40 +0530

    updated sigin and signup for manager

--- app/models.py
+++ app/models.py
@@ -1,15 +1,21 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, TIMESTAMP, Boolean, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, TIMESTAMP, Boolean, Float, DateTime
 from sqlalchemy.sql import func
 from .db import Base
 
+
+
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100))
-    email = Column(String(150))
-    password_hash = Column(String(256))
-    role = Column(String(20))
-    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+    is_approved = Column(Boolean, default=False, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
 class Employee(Base):
     __tablename__ = "employees"
